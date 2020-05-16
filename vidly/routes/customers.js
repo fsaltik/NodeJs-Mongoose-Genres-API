9dadc260d54bbdaea39ec31635fc42ba20776676
Firat Saltik <f_saltik@yahoo.com>
2020-05-16 23:53:32 +0300

    Build the Customers API

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -41,6 +41,38 @@ router.post('/', async (req, res) => {
   res.send(customer);
 });
 
+router.put('/:id', async (req, res) => {
+    const { error } = validateCustomer(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const customer = await Customer.findByIdAndUpdate(req.params.id,
+      { 
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone
+      }, { new: true });
+  
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+    
+    res.send(customer);
+  });
+  
+  router.delete('/:id', async (req, res) => {
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+  
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+  
+    res.send(customer);
+  });
+  
+  router.get('/:id', async (req, res) => {
+    const customer = await Customer.findById(req.params.id);
+  
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+  
+    res.send(customer);
+  });
+
 function validateCustomer(genre) {
     const schema = {
       name: Joi.string().min(5).max(50).required(),
